56fe58076f06fb2e54f2dedd1d1c431b764c7c6b
Antonio Mejia <58567224+avmejia-tec@users.noreply.github.com>
2024-08-12 16:26:49 +0000

    Sesion2-|

---
+++ Sesion2-1/variables.py
@@ -0,0 +1,9 @@
+import math
+
+#est es un comentario
+ent = float(input(" DAme un valor flotante  : "))
+#ent = float(ent)
+print(ent)
+print(type(ent))
+print("LA variables ent tiene como valor : " +str(ent) )
+print(f'LA variable x es : {ent + 1}')
