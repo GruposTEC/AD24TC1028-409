c92307fd4708fd586606c53d1d153f7e387f61fe
Antonio Mejia <58567224+avmejia-tec@users.noreply.github.com>
2024-08-19 15:19:46 +0000

    quiz1

---
+++ Decisiones/quiz1.py
@@ -0,0 +1,7 @@
+import math
+x1 = float(input("Dame el numero : "))
+x2 = float(input("Dame el numero : "))
+diferencia = math.pow(x1,2)-math.pow(x2,2)
+resultado = math.sqrt(diferencia)
+print("El resultado es :")
+print(resultado)
